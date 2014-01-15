1446d762887932c2a49808a6e9d496111ecbb660
Stephen Compton <sccompton@me.com>
2014-01-15 14:24:29 -0600

    Initial Commit

---
+++ Farkle/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Farkle
+//
+//  Created by Stephen Compton on 1/15/14.
+//  Copyright (c) 2014 Stephen Compton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
