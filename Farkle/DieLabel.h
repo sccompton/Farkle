3b4a101dedaac65d838a727446db142a14a9c48f
Stephen Compton <sccompton@me.com>
2014-01-15 18:06:58 -0600

    first commit

---
+++ Farkle/DieLabel.h
@@ -0,0 +1,13 @@
+//
+//  DieLabel.h
+//  Farkle
+//
+//  Created by Stephen Compton on 1/15/14.
+//  Copyright (c) 2014 Stephen Compton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DieLabel : UILabel
+
+@end
---
+++ Farkle/DieLabelDelegate.h
@@ -0,0 +1,13 @@
+//
+//  DieLabelDelegate.h
+//  Farkle
+//
+//  Created by Stephen Compton on 1/15/14.
+//  Copyright (c) 2014 Stephen Compton. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol DieLabelDelegate <NSObject>
+
+@end
